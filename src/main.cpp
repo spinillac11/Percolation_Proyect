94d41ebf74b5e341bd6847a857d70aa02d008517
spinillac11 <spinillac@unal.edu.co>
2025-06-04 21:11:08 -0500

    Problema de comparación de int con unsigned int resuelto

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,46 @@
-#include "functions.h"
-#include "UnionFind.h"
-#include "vec_maps.h"
+#include "declarations.h"
 
-#include <cstdlib>
-#include <iostream>
-
-int main (int argc, char **argv){
-    
+int main(int argc, char **argv) {
     // read parameters
-    const int L = std::atoi(argv[1]); // Grid size
-    const double p = std::atof(argv[2]); // Filling probability 0 \leq p \leq 1
+    const int L = std::atoi(argv[1]);
+    const double P = std::atof(argv[2]); 
+
+    // Generate Lattice
+    Vec Lattice(L*L, 0);
     
-    // Generate lattice
-    Vec lattice (L*L, 0);
-
     // Generate map for cluster size
-    Map clusterSize;
+    Map cluster_size;   
 
-    // Fill lattice
-    fillLaticce(lattice, p);
+    // Fill Lattice 
+    fill_laticce(Lattice, P);
 
     // Print Lattice in console  
     for (int y = 0; y < L; ++y) {
         for (int x = 0; x < L; ++x) {
-            std::cout << lattice[y * L + x] << ' ';
+            std::cout << Lattice[y * L + x] << ' ';
         }
         std::cout << '\n';
     } 
     std::cout << '\n';
 
-    // Find clusters
-    findClusters(lattice, clusterSize);
+    // Find clusters 
+    cluster_size = find_clusters(Lattice); 
 
-    // Save lattice in a .txt
-    saveLattice(lattice);
+    // Print Lattice with clusters
+    print(Lattice); 
 
     // Print each cluster size
-    std::cout << "Cluster" << "\t" << "size:" << std::endl;
-    for (const auto& [cluster, size] : clusterSize) {
-        std::cout << cluster << "\t" << size << std::endl;
+    std::cout << "Cluster size:" << std::endl;
+    for (const auto & [cluster, size] : cluster_size) {
+        std::cout << cluster << "\t" << size << std::endl; 
     }
+
+    Vec percol = detec_perc(Lattice);
+
+    std::cout << "The Clusters that percolate are:" << std::endl;
+    for (size_t i = 0; i < percol.size(); i++) {
+        std::cout << percol[i] << std::endl;
+    } 
 
     return 0;
 }
