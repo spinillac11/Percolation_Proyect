065a9f50ebab8e90102e73418ebf2f8e9674fd00
spinillac11 <spinillac@unal.edu.co>
2025-06-10 21:45:15 -0500

    Path correction

--- graphics/plot_opti.py
+++ graphics/plot_opti.py
@@ -1,10 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import os
 
-# Paths
-data_dir = "/home/ertorreso/2025-I-Intro/Proyectos/Percolation_Proyect/data"
-figures_dir = "/home/ertorreso/2025-I-Intro/Proyectos/Percolation_Proyect/figures"
 
 # File names and corresponding labels
 files = [
@@ -21,8 +17,7 @@ colors = ['blue', 'green', 'orange', 'red', 'purple']
 plt.figure(figsize=(10, 6))
 
 for file, label, color in zip(files, labels, colors):
-    filepath = os.path.join(data_dir, file)
-    data = np.loadtxt(filepath)
+    data = np.loadtxt("data/"+file)
     x = data[:, 0]
     mean = data[:, 1] / 1000       # Convert to ms
     std = data[:, 2] / 1000        # Convert to ms
@@ -36,10 +31,6 @@ plt.legend()
 plt.grid(True, which='both', linestyle='--', linewidth=0.5)
 plt.tight_layout()
 
-# Make sure figures directory exists
-os.makedirs(figures_dir, exist_ok=True)
 
-# Save the plot
-output_path = os.path.join(figures_dir, "opti_comparison.pdf")
-plt.savefig(output_path)
-print(Figura guardada en opti_comparison.pdf)
+plt.savefig("figures/opti_comparison.pdf")
+print("Figura guardada en opti_comparison.pdf")
