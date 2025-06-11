1d7789baf607e296da869dc66b7e1fa2cd29d5f6
Bryan_Anaya <banaya@unal.edu.co>
2025-06-11 00:11:08 -0500

    Scripts to organize reports and Makefile added, gitignore edited

---
+++ script/organize_report_perf.sh
@@ -0,0 +1,19 @@
+dir="out_report"
+input_perf="$dir/report_perf.txt"
+output_perf="$dir/report_perf_filtered.txt"
+
+# Filtrar perf
+
+## Lista de funciones a procesar
+functions=("fill_laticce" "print" "Find" "Union" "HoshenKopelman" "find_clusters" "detec_perc")
+
+## 1. Encabezado
+head -n 11 "$input_perf" > "$output_perf"
+
+## 2. Añadir la primera ocurrencia de cada función
+for fname in "${functions[@]}"; do
+    grep "$fname" "$input_perf" | head -n 1
+done | sort -k1,1nr >> "$output_perf"   # Ordenar por columna 1 numérica descendente
+
+# 3. fin
+echo -e "\n# --- Fin del reporte filtrado (perf) ---" >> "$output_perf"
