42cc5e447eb32738b98fd7785ca6ef94be6b9d12
Erick <ertorreso@unal.edu.co>
2025-06-11 14:48:37 -0500

    Adding comments to scripts

--- script/analysis.sh
+++ script/analysis.sh
@@ -1,49 +1,59 @@
+# Set output directory
 dir="data";
+
+# List of lattice sizes to simulate
 Ls="16 32 64 128 256 512";
+
+# Define probabilities p to simulate
+# Fine-grained step around critical region (~0.59)
 Ps="$(seq 0 0.06 0.54) $(seq 0.55 0.01 0.65) $(seq 0.66 0.04 1) 1";
 
+# For each combination of L and p, run program.x 10 times and append to data_L.txt
 parallel --progress './program.x {1} {2} >> data_{1}.txt' ::: $Ls ::: $Ps ::: $(seq 1 1 10);
 
+# Compute mean and stddev of percolation probability for each L
 for L in $Ls; do
     awk -F'\t' '
     {
-        key = $1 "\t" $2;
-        count[key]++;
-        sum[key] += $3;
-        xsquare[key] += ($3)^2;
+        key = $1 "\t" $2;                           # Create key based on L and p
+        count[key]++;                               # Count number of samples for this key
+        sum[key] += $3;                             # Sum percolation result (0 or 1)
+        xsquare[key] += ($3)^2;                     # For standard deviation
     }
     END {
         for (k in sum) {
-            N = count[k];
-            mean = sum[k]/N;
-            std = sqrt(xsquare[k] / N - mean^2);
-            printf "%s\t%.3f\t%.3f\n", k, mean, std;
+            N = count[k];                           # Total number of samples
+            mean = sum[k]/N;                        # Calculate average
+            std = sqrt(xsquare[k] / N - mean^2);    # Calculate stddev using variance formula
+            printf "%s\t%.3f\t%.3f\n", k, mean, std; # Output result
         }
     }
     ' data_${L}.txt | sort -k1,1n -k2,2n > ${dir}/perc_prob_${L}.txt;
+    # Output: columns [L, p, mean_perc, std_perc]
 done
-
+# Filter data where percolation occurred (p = 1)
+# This keeps only rows where the system percolated
 for L in $Ls; do
     awk '
     {if($3 == 1) print $0}
     ' data_${L}.txt > data_percol_${L}.txt;
-    rm -f data_${L}.txt;
+    rm -f data_${L}.txt; # Clean up intermediate file
 done
-
+# Compute mean and stddev of percolating cluster size (S)
 for L in $Ls; do
     awk -F'\t' '
     {
-        key = $1 "\t" $2;
-        count[key]++;
-        sum[key] += $5;
-        xsquare[key] += ($5)^2;
+        key = $1 "\t" $2;                           # use L and p as key
+        count[key]++;                               # count entries
+        sum[key] += $5;                             # sum of percolating cluster size S
+        xsquare[key] += ($5)^2;                     # for standard deviation
     }
     END {
         for (k in sum) {
-            N = count[k];
-            mean = sum[k]/N;
-            std = sqrt(xsquare[k] / N - mean^2);
-            printf "%s\t%.3f\t%.3f\n", k, mean, std;
+            N = count[k];                           # Total number of samples
+            mean = sum[k]/N;                        # Calculate average
+            std = sqrt(xsquare[k] / N - mean^2);    # Calculate stddev using variance formula
+            printf "%s\t%.3f\t%.3f\n", k, mean, std; # Output result
         }
     }
     ' data_percol_${L}.txt | sort -k1,1n -k2,2n > ${dir}/perc_size_${L}.txt;
--- script/optimization.sh
+++ script/optimization.sh
@@ -1,28 +1,36 @@
+# Set output directory
 dir="data";
+
+# List of optimization levels
 OpL="0 1 2 3 fast"
+
+# List of lattice sizes
 Ls="16 32 64 128 256 512 1024 2048";
 
+# Compile the program with different optimization flags (-O0, -O1, ..., -Ofast)
 for Op in $OpL; do
     echo "Compilando con -O$Op..."
     g++ -std=c++17 -O$Op -Iinclude source/main.cpp source/functions.cpp -o O$Op.x
 done
 
+# Run each compiled binary for each lattice size 10 times in parallel
 parallel --progress './O{1}.x {2} 0.6 >> data_O{1}.txt' ::: $OpL ::: $Ls ::: $(seq 1 1 10);
 
+# Aggregate and compute mean and std deviation of column 6 (execution time) for each L
 for Op in $OpL; do
     awk -F'\t' '
     {
-        key = $1;
-        count[key]++;
-        sum[key] += $6;
-        xsquare[key] += ($6)^2;
+        key = $1;                                   # Use lattice size as key
+        count[key]++;                               # Count the number of repetitions
+        sum[key] += $6;                             # Sum the values in column 6 (execution time)
+        xsquare[key] += ($6)^2;                     # For standar deviation
     }
     END {
         for (k in sum) {
-            N = count[k];
-            mean = sum[k]/N;
-            std = sqrt(xsquare[k] / N - mean^2);
-            printf "%s\t%.3f\t%.3f\n", k, mean, std;
+            N = count[k];                           # Total number of samples
+            mean = sum[k]/N;                        # Mean of execution time
+            std = sqrt(xsquare[k] / N - mean^2);    # Calculate stddev using variance formula
+            printf "%s\t%.3f\t%.3f\n", k, mean, std; 
         }
     }
     ' data_O${Op}.txt | sort -k1,1n -k2,2n > ${dir}/opti_O${Op}.txt;
